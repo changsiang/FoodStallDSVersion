e30154a33f9042f2b790f20a9ef3f20a2c0ab667
hikari8807 <checkingmymail@gmail.com>
2017-11-12 14:03:30 +0800

    First Commit

---
+++ FoodStallDSVersion/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace FoodStallDSVersion
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DataSet1 ds;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cnS = "Server=tcp:changsiangsqlserver.database.windows.net,1433;Initial Catalog=changsiangfooddb;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection cn = new SqlConnection(cnS);
+            SqlCommand cm = new SqlCommand("Select * from Users", cn);
+            SqlDataAdapter da = new SqlDataAdapter(cm);
+            ds = new DataSet1();
+            da.Fill(ds, "Users");
+            
+        }
+    }
+}
